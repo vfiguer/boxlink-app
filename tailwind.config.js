eb1d26bfa939bb5a1ad365e89721648f43e6a486
vfiguer <136345153+vfiguer@users.noreply.github.com>
2025-05-22 00:09:27 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Cambia si tienes otra estructura
+  theme: {
+    extend: {
+      fontFamily: {
+        lato: ['Lato', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
